c58b52f0a1849748aa3cace1a4d770a6ca00bf76
TrungMai <bieuluu53@gmail.com>
2025-07-10 18:23:29 +0700

    create draft of analytics.js

---
+++ server/routes/analytics.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/api/analytics/category-summary', (req, res) => {})
+
+
+router.get('/api/analytics/category-summary', (req, res) => {})
+
+module.exports = router;
