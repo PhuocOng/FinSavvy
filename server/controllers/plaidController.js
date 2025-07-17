336804d725126b3d8cb5c5e6f91b2977a262f741
Quang D. Nguyen <quang13012005@gmail.com>
2025-07-17 17:59:57 +0700

    added getTransactions function

--- server/controllers/plaidController.js
+++ server/controllers/plaidController.js
@@ -1,7 +1,7 @@
 import { plaidClient  } from "../services/plaidService.js";
 
 // POST /api/plaid/exchange_token
-export const exchangeToken = async (req, res) => {
+const exchangeToken = async (req, res) => {
   const { public_token } = req.body
   try {
     const response = await plaidClient.itemPublicTokenExchange({
@@ -14,4 +14,28 @@ export const exchangeToken = async (req, res) => {
   }
 }
 
+// GET /api/plaid/transactions
+export const getTransactions = async (req, res) => {
+  const { access_token } = req.query;
 
+  if (!access_token) {
+    return res.status(400).json({ error: 'Missing access_token' });
+  }
+
+  // change the hardcode later
+  const startDate = '2024-01-01'; 
+  const endDate = '2024-12-31';
+
+  try {
+    const response = await plaidClient.transactionsGet({
+      access_token,
+      start_date: startDate,
+      end_date: endDate,
+      options: { count: 100, offset: 0 },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch transactions' });
+  }
+};
