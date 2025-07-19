16d33fe2ce974ab170b03cd672ef8d1e53d784db
Quang D. Nguyen <quang13012005@gmail.com>
2025-07-19 21:07:54 +0700

    commented nodeMailer functions

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -127,7 +127,7 @@ const sendVerifyOtp = async (req, res) => {
             text: `Your OTP is ${otp}. Verify your account using this OTP`, html: EMAIL_VERIFY_TEMPLATE.replace("{{otp}}", otp).replace("{{email}}", user.email)
         }
 
-        await transporter.sendMail(mailOption)
+        // await transporter.sendMail(mailOption)
 
         return res.json({ success: true, message: "OTP sent to email successfully" });
 
