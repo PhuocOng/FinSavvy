1d7b6b02bbaeda1644bfeed9fe74ad2d1e07b9c5
Quang D. Nguyen <quang13012005@gmail.com>
2025-07-19 21:04:27 +0700

    deleted

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
+
   app.listen(PORT, () => {
     console.log(`🚀 Server is running on port ${PORT}`);
     console.log(`📡 API endpoints:`);
