7e70f299f1649303450d5554fc4d613395997f10
Quang D. Nguyen <quang13012005@gmail.com>
2025-07-19 21:19:45 +0700

    changed connection string name for connection

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@
 
  const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Connected mongoDB`)
     } catch (error) {
         console.log(`Mongo connection error`, error);
