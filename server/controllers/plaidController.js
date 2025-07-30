ab91dafdb963ba7ba4e25aea64514411a0966d1f
TrungMai <bieuluu53@gmail.com>
2025-07-30 16:28:37 +0700

    update plaidController with link_token and public_token

--- server/controllers/plaidController.js
+++ server/controllers/plaidController.js
@@ -1,18 +1,52 @@
-const { plaidClient } = require("../services/plaidService.js");
+const plaidClient = require('../config/plaid');
+const User = require('../models/User');
 
-// POST /api/plaid/exchange_token
-const exchangeToken = async (req, res) => {
-  const { public_token } = req.body
+// Create a link_token for the client to use
+const createLinkToken = async (req, res) => {
+  // The user's ID should be available from your JWT authentication middleware
+  const clientUserId = req.user.id;
+
+  const request = {
+    user: { client_user_id: clientUserId },
+    client_name: 'FinSavvy',
+    products: ['transactions'],
+    country_codes: ['US'],
+    language: 'en',
+  };
+
+  try {
+    const createTokenResponse = await plaidClient.linkTokenCreate(request);
+    res.json(createTokenResponse.data);
+  } catch (error) {
+    console.error("Error creating link token:", error.response.data);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Exchange the public_token from the client for an access_token
+const exchangePublicToken = async (req, res) => {
+  const { public_token } = req.body;
+  const userId = req.user.id;
+
   try {
     const response = await plaidClient.itemPublicTokenExchange({
-      public_token,
-    }); 
+      public_token: public_token,
+    });
+
     const { access_token, item_id } = response.data;
-    res.json({ access_token, item_id });
+
+    // Save the accessToken and itemId to the user in your database
+    await User.findByIdAndUpdate(userId, {
+      plaidAccessToken: access_token,
+      plaidItemId: item_id,
+    });
+
+    res.json({ message: 'Bank account linked successfully!' });
   } catch (error) {
-    res.status(500).json({ error: "Failed to exchange token"});
+    console.error("Error exchanging public token:", error.response.data);
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
 // GET /api/plaid/transactions
 const getTransactions = async (req, res) => {
@@ -41,4 +75,4 @@ const getTransactions = async (req, res) => {
 };
 
 
-module.exports = { exchangeToken, getTransactions };
+module.exports = { createLinkToken, exchangePublicToken, getTransactions };
