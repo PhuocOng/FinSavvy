68523c894bde656c87cd268683528b944991868e
levanduc <a519levanduc@gmail.com>
2025-08-16 11:18:37 -0500

    delete search bar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -94,14 +94,13 @@ const Navbar = () => {
 
         {/* 2. Group the right-side items for easy layout */}
         <div className="flex items-center gap-4">
-          <div className="relative hidden md:flex items-center justify-center">
+          {/* <div className="relative hidden md:flex items-center justify-center">
             <i className="bx bx-search absolute left-3 text-2xl text-gray-500"></i>
-            {/* Add dark mode styles to the search input */}
             <input type="text" placeholder="Search..." className="py-2 pl-10 rounded-xl border-2 border-[#2563eb] focus:bg-slate-100 dark:bg-gray-700 dark:text-white dark:border-blue-500 focus:outline-[#3b82f6]" />
-          </div>
+          </div> */}
 
           {/* 3. Add the ThemeToggleButton here */}
-          <ThemeToggleButton />
+          <ThemeToggleButton/>
 
           {userData && (
             <div className="relative" ref={profileDropdownRef}>
